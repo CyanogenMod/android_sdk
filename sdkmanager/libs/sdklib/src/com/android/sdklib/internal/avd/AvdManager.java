44c3abd81d6ed2a715bc693e2ec74f009e9a1bb2
Raphael Moll <ralf@android.com>
2011-03-22 11:10:53 -0700

    Fix AvdManager to correctly find emulator in AOSP.
    This is a temporary workaround on the lack of a main
    "emulator" binary in AOSP, which will be fixed later.
    
    Change-Id: I61f03ed8a54a410469f95a1100ef38eef7ab4bf6

--- sdkmanager/libs/sdklib/src/com/android/sdklib/internal/avd/AvdManager.java
+++ sdkmanager/libs/sdklib/src/com/android/sdklib/internal/avd/AvdManager.java
@@ -308,12 +308,28 @@ public final class AvdManager {
 
             // Start with base name of the emulator
             path = path + SdkConstants.FN_EMULATOR;
+
             // If not using ARM, add processor type to emulator command line
-            if (!getAbiType().equalsIgnoreCase(SdkConstants.ABI_ARMEABI)) {
+            boolean useAbi = !getAbiType().equalsIgnoreCase(SdkConstants.ABI_ARMEABI);
+
+            if (useAbi) {
                 path = path + "-" + getAbiType();
             }
             // Add OS appropriate emulator extension (e.g., .exe on windows)
             path = path + SdkConstants.FN_EMULATOR_EXTENSION;
+
+            // HACK: The AVD manager should look for "emulator" or for "emulator-abi" (if not arm).
+            // However this is a transition period and we don't have that unified "emulator" binary
+            // in AOSP so if we can't find the generic one, look for an abi-specific one with the
+            // special case that the armeabi one is actually named emulator-arm.
+            // TODO remove this kludge once no longer necessary.
+            if (!useAbi && !(new File(path).isFile())) {
+                path = sdkPath + SdkConstants.OS_SDK_TOOLS_FOLDER;
+                path = path + SdkConstants.FN_EMULATOR;
+                path = path + "-" + getAbiType().replace(SdkConstants.ABI_ARMEABI, "arm"); //$NON-NLS-1$
+                path = path + SdkConstants.FN_EMULATOR_EXTENSION;
+            }
+
             return path;
         }
 
