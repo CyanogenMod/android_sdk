00855d474747db10f06b1f66fd0cf0eb23f69e4b
Tor Norbye <tnorbye@google.com>
2011-05-26 18:55:26 -0700

    Handle fragment tags in included contexts
    This changeset fixes the support for rendering user-chosen layouts in
    <fragment> tags in a couple of scenarios:
    - where the fragment is part of a layout that "surrounds" the current
      layout (e.g. "show included in")
    - where the fragment is part of a layout that is included from the
      edited layout
    
    In both cases, the fragment is read from a layout that is parsed with
    a plain pull parser rather than the full XML node model used for
    edited layouts, so to fix this the ContextPullParser now looks
    specially for <fragment> tags and handles these specially.
    
    In the second scenario, the parser is constructed by layoutlib, so we
    have to use the project callback to anticipate requested parsers and
    supply our own fragment-capable pull parsers instead.
    
    Change-Id: I8aba253ac0c1a0d5dad7bc6efb9def5f20c75144

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/layout/ContextPullParser.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/layout/ContextPullParser.java
@@ -20,28 +20,45 @@ import static com.android.ide.common.layout.LayoutConstants.ATTR_LAYOUT_HEIGHT;
 import static com.android.ide.common.layout.LayoutConstants.ATTR_LAYOUT_WIDTH;
 import static com.android.ide.common.layout.LayoutConstants.VALUE_FILL_PARENT;
 import static com.android.ide.common.layout.LayoutConstants.VALUE_MATCH_PARENT;
+import static com.android.ide.eclipse.adt.internal.editors.layout.descriptors.LayoutDescriptors.ATTR_LAYOUT;
+import static com.android.ide.eclipse.adt.internal.editors.layout.descriptors.LayoutDescriptors.VIEW_FRAGMENT;
+import static com.android.ide.eclipse.adt.internal.editors.layout.descriptors.LayoutDescriptors.VIEW_INCLUDE;
+import static com.android.ide.eclipse.adt.internal.editors.layout.gle2.LayoutMetadata.KEY_FRAGMENT_LAYOUT;
 
 import com.android.ide.common.rendering.api.ILayoutPullParser;
 import com.android.ide.common.rendering.api.IProjectCallback;
-import com.android.layoutlib.api.IXmlPullParser;
+import com.android.ide.eclipse.adt.AdtPlugin;
+import com.android.ide.eclipse.adt.internal.editors.layout.gle2.LayoutMetadata;
 import com.android.sdklib.SdkConstants;
 
 import org.kxml2.io.KXmlParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.File;
 
 /**
- * Modified {@link KXmlParser} that adds the methods of {@link ILayoutPullParser}.
+ * Modified {@link KXmlParser} that adds the methods of {@link ILayoutPullParser}, and
+ * performs other layout-specific parser behavior like translating fragment tags into
+ * include tags.
  * <p/>
  * It will return a given parser when queried for one through
- * {@link IXmlPullParser#getParser(String)} for a given name.
+ * {@link ILayoutPullParser#getParser(String)} for a given name.
  *
  */
 public class ContextPullParser extends KXmlParser implements ILayoutPullParser {
-
+    private static final String COMMENT_PREFIX = "<!--"; //$NON-NLS-1$
+    private static final String COMMENT_SUFFIX = "-->"; //$NON-NLS-1$
+    /** The callback to request parsers from */
     private final IProjectCallback mProjectCallback;
+    /** The {@link File} for the layout currently being parsed */
+    private File mFile;
+    /** The layout to be shown for the current {@code <fragment>} tag. Usually null. */
+    private String mFragmentLayout = null;
 
-    public ContextPullParser(IProjectCallback projectCallback) {
+    public ContextPullParser(IProjectCallback projectCallback, File file) {
         super();
         mProjectCallback = projectCallback;
+        mFile = file;
     }
 
     // --- Layout lib API methods
@@ -62,7 +79,28 @@ public class ContextPullParser extends KXmlParser implements ILayoutPullParser {
     // --- KXMLParser override
 
     @Override
+    public String getName() {
+        String name = super.getName();
+
+        // At designtime, replace fragments with includes.
+        if (name.equals(VIEW_FRAGMENT)) {
+            mFragmentLayout = findFragmentLayout();
+            if (mFragmentLayout != null) {
+                return VIEW_INCLUDE;
+            }
+        } else {
+            mFragmentLayout = null;
+        }
+
+        return name;
+    }
+
+    @Override
     public String getAttributeValue(String namespace, String localName) {
+        if (localName.equals(ATTR_LAYOUT) && mFragmentLayout != null) {
+            return mFragmentLayout;
+        }
+
         String value = super.getAttributeValue(namespace, localName);
 
         // on the fly convert match_parent to fill_parent for compatibility with older
@@ -75,5 +113,59 @@ public class ContextPullParser extends KXmlParser implements ILayoutPullParser {
         }
 
         return value;
+    }
+
+    /**
+     * This method determines whether the {@code <fragment>} tag in the current parsing
+     * context has been configured with a layout to render at designtime. If so,
+     * it returns the resource name of the layout, and if not, returns null.
+     */
+    private String findFragmentLayout() {
+        try {
+            if (!isEmptyElementTag()) {
+                // We need to look inside the <fragment> tag to see
+                // if it contains a comment which indicates a fragment
+                // to be rendered.
+                String file = AdtPlugin.readFile(mFile);
+
+                int line = getLineNumber() - 1;
+                int column = getColumnNumber() - 1;
+                int offset = 0;
+                int currentLine = 0;
+                int length = file.length();
+                while (currentLine < line && offset < length) {
+                    int next = file.indexOf('\n', offset);
+                    if (next == -1) {
+                        break;
+                    }
+
+                    currentLine++;
+                    offset = next + 1;
+                }
+                if (currentLine == line) {
+                    offset += column;
+                    if (offset < length) {
+                        offset = file.indexOf('<', offset);
+                        if (offset != -1 && file.startsWith(COMMENT_PREFIX, offset)) {
+                            // The fragment tag contains a comment
+                            int end = file.indexOf(COMMENT_SUFFIX, offset);
+                            if (end != -1) {
+                                String commentText = file.substring(
+                                        offset + COMMENT_PREFIX.length(), end);
+                                String l = LayoutMetadata.getProperty(KEY_FRAGMENT_LAYOUT,
+                                        commentText);
+                                if (l != null) {
+                                    return l;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (XmlPullParserException e) {
+            AdtPlugin.log(e, null);
+        }
+
+        return null;
     }
 }
