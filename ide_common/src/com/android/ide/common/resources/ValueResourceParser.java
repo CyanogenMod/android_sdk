df07f53c944af94abb91d2164c7e79d5b02697b9
Xavier Ducrohet <xav@android.com>
2011-06-14 18:13:11 -0700

    Fix parsing of the styleable enum/flag attribute values.
    Better handling for hexa value > 0x7FFFFFFF
    
    Change-Id: If0c2ed23847a720431bceee95f8b264ceb967e3e

--- ide_common/src/com/android/ide/common/resources/ValueResourceParser.java
+++ ide_common/src/com/android/ide/common/resources/ValueResourceParser.java
@@ -150,8 +150,10 @@ public final class ValueResourceParser extends DefaultHandler {
                 String value = attributes.getValue(ATTR_VALUE);
 
                 try {
+                    // Integer.decode/parseInt can't deal with hex value > 0x7FFFFFFF so we
+                    // use Long.decode instead.
                     mCurrentDeclareStyleable.addValue(mCurrentAttribute,
-                            name, Integer.decode(value));
+                            name, (int)(long)Long.decode(value));
                 } catch (NumberFormatException e) {
                     // pass, we'll just ignore this value
                 }
