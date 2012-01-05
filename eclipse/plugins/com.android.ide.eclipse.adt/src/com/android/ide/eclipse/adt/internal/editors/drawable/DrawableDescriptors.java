433186abd8111e3a25373b2d966ad05a5fe9d4a5
Tor Norbye <tnorbye@google.com>
2012-01-04 16:11:51 -0800

    Hide the animated-rotate metadata from the drawable descriptors
    This changeset removes the metadata for <animated-rotate>
    drawables. Turns out the underlying class,
    android.graphics.drawable.AnimatedRotateDrawable, is marked
    hidden. Therefore, the <animated-rotate> and its associated attributes
    should not be shown to the user since they cannot be used. This is the
    subject of issue http://b.android.com/19248 .
    
    While looking at this I added some instrumentation to the addElement
    method (which adds the manually registered style classes and their
    attributes). This validates that all added attributes are referenced
    in the public.xml file. The above <animated-rotate> class is the only
    class that was not public. However, that's only among the manually
    registered metadata for animators, drawables and color lists.  For the
    overall attributes (on layouts etc), there are many attributes picked
    up from the attrs.xml file that do not appear in public.xml.  In a
    subsequent CL we need to add filtering to that import phase as well.
    
    Change-Id: Ia6fba23ca47b12cc9a13cc7e203b6d5849dfe103

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/drawable/DrawableDescriptors.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/drawable/DrawableDescriptors.java
@@ -96,12 +96,15 @@ public class DrawableDescriptors implements IDescriptorProvider {
             SDK_URL_BASE + "animation-resource.html#Frame",
             xmlns, null, true /*mandatory*/);
 
+        /* For some reason, android.graphics.drawable.AnimatedRotateDrawable is marked with @hide
+         * so we should not register it and its attributes here. See issue #19248 for details.
         AnimatorDescriptors.addElement(descriptors, styleMap,
             "animated-rotate", "Animated Rotate", "AnimatedRotateDrawable", null, //$NON-NLS-1$ //$NON-NLS-3$
             // Need docs
-            null /* tooltip */,
-            null /* sdk_url */,
-            xmlns, null, true /*mandatory*/);
+            null, // tooltip
+            null, // sdk_url
+            xmlns, null, true);
+        */
 
         AnimatorDescriptors.addElement(descriptors, styleMap,
             "bitmap", "BitMap", "BitmapDrawable", null, //$NON-NLS-1$ //$NON-NLS-3$
@@ -118,7 +121,6 @@ public class DrawableDescriptors implements IDescriptorProvider {
                 + "this Drawable's current level value.",
             SDK_URL_BASE + "drawable-resource.html#Clip", //$NON-NLS-1$
             xmlns, null, true /*mandatory*/);
-
 
         AnimatorDescriptors.addElement(descriptors, styleMap,
             "color", "Color", "ColorDrawable", null, //$NON-NLS-1$ //$NON-NLS-3$
