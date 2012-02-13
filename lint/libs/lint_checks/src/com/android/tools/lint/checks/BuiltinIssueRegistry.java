b89f2aa44747994f8a190cf0da7c466cadb9da2b
Tor Norbye <tnorbye@google.com>
2012-02-13 10:21:50 -0800

    Add namespace-typo detector
    This changeset adds a new lint detector which looks for typos in the
    namespace declaration for Android. If it sees the prefix "android" it
    ensures that the URI matches exactly (and case-sensitively)
       http://schemas.android.com/apk/res/android
    
    If the prefix is "a", it also checks for the above prefix, but only
    warns if the difference is "close" (edit distance <= 3) such that
    it belives it's a typo, not some unrelated prefix bound to "a".
    
    (This was requested in http://b.android.com/25449)
    
    Change-Id: Ic6dee77a300fc048cb9bee3e455f533b858ef6bb

--- lint/libs/lint_checks/src/com/android/tools/lint/checks/BuiltinIssueRegistry.java
+++ lint/libs/lint_checks/src/com/android/tools/lint/checks/BuiltinIssueRegistry.java
@@ -125,6 +125,7 @@ public class BuiltinIssueRegistry extends IssueRegistry {
         issues.add(ViewTypeDetector.ISSUE);
         issues.add(WrongImportDetector.ISSUE);
         issues.add(ViewConstructorDetector.ISSUE);
+        issues.add(TypoDetector.ISSUE);
         issues.add(AlwaysShowActionDetector.ISSUE);
 
         addCustomIssues(issues);
