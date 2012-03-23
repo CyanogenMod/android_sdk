f4a36134b37fca99ec0fad21e1f67096ef655ee0
Raphael Moll <raphael@google.com>
2012-03-23 11:30:57 -0700

    Fix build: Entirely disable the SDK Monitor RCP.
    Apparently removing from the SDK build it not enough.
    
    Change-Id: I3aee0ee0eb05d83ea91d3ad75493185c5d756e38

--- eclipse/scripts/rcp/Android.mk
+++ eclipse/scripts/rcp/Android.mk
@@ -1,41 +1,41 @@
 # Copyright 2012 The Android Open Source Project
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := monitor
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_TAGS := optional
-LOCAL_IS_HOST_MODULE := true
-include $(BUILD_SYSTEM)/base_rules.mk
-
-RCP_MONITOR_DIR := $(TOPDIR)out/host/eclipse/rcp/build/I.RcpBuild
-
-define mk-rcp-monitor-atree-file
-    srczip=$(RCP_MONITOR_DIR)/RcpBuild-$(1).$(2).zip && \
-    dstdir=$(HOST_OUT)/eclipse/monitor-$(1).$(2) && \
-    rm -rf $(V) $$dstdir && \
-    mkdir -p $$dstdir && \
-    unzip -q $$srczip -d $$dstdir
-endef
-
-# The RCP monitor. It is referenced by build/target/products/sdk.mk
-$(LOCAL_BUILT_MODULE) : $(TOPDIR)sdk/eclipse/scripts/rcp/monitor \
-			$(TOPDIR)sdk/eclipse/scripts/rcp/build.xml \
-			$(TOPDIR)sdk/eclipse/scripts/rcp/build.properties \
-			$(shell $(TOPDIR)sdk/eclipse/scripts/create_all_symlinks.sh -d)
-	@mkdir -p $(dir $@)
-	$(hide)$(TOPDIR)sdk/eclipse/scripts/create_all_symlinks.sh -c
-	$(hide)cd $(TOPDIR)sdk/eclipse/scripts/rcp && ant -DbuildFor=$(HOST_OS)
-	$(hide)cp $(V) $(TOPDIR)sdk/eclipse/scripts/rcp/monitor $@
-	$(hide)if [[ $(HOST_OS) == "linux" ]]; then \
-		$(call mk-rcp-monitor-atree-file,linux.gtk,x86)    ; \
-		$(call mk-rcp-monitor-atree-file,linux.gtk,x86_64) ; \
-	fi
-	$(hide)if [[ $(HOST_OS) == "darwin" ]]; then \
-		$(call mk-rcp-monitor-atree-file,macosx.cocoa,x86_64) ; \
-	fi
-	$(hide)if [[ $(HOST_OS) == "windows" ]]; then \
-		$(call mk-rcp-monitor-atree-file,win32.win32,x86)    ; \
-		$(call mk-rcp-monitor-atree-file,win32.win32,x86_64) ; \
-	fi
+#LOCAL_PATH := $(call my-dir)
+#include $(CLEAR_VARS)
+#
+#LOCAL_MODULE := monitor
+#LOCAL_MODULE_CLASS := EXECUTABLES
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_IS_HOST_MODULE := true
+#include $(BUILD_SYSTEM)/base_rules.mk
+#
+#RCP_MONITOR_DIR := $(TOPDIR)out/host/eclipse/rcp/build/I.RcpBuild
+#
+#define mk-rcp-monitor-atree-file
+#    srczip=$(RCP_MONITOR_DIR)/RcpBuild-$(1).$(2).zip && \
+#    dstdir=$(HOST_OUT)/eclipse/monitor-$(1).$(2) && \
+#    rm -rf $(V) $$dstdir && \
+#    mkdir -p $$dstdir && \
+#    unzip -q $$srczip -d $$dstdir
+#endef
+#
+## The RCP monitor. It is referenced by build/target/products/sdk.mk
+#$(LOCAL_BUILT_MODULE) : $(TOPDIR)sdk/eclipse/scripts/rcp/monitor \
+#			$(TOPDIR)sdk/eclipse/scripts/rcp/build.xml \
+#			$(TOPDIR)sdk/eclipse/scripts/rcp/build.properties \
+#			$(shell $(TOPDIR)sdk/eclipse/scripts/create_all_symlinks.sh -d)
+#	@mkdir -p $(dir $@)
+#	$(hide)$(TOPDIR)sdk/eclipse/scripts/create_all_symlinks.sh -c
+#	$(hide)cd $(TOPDIR)sdk/eclipse/scripts/rcp && ant -DbuildFor=$(HOST_OS)
+#	$(hide)cp $(V) $(TOPDIR)sdk/eclipse/scripts/rcp/monitor $@
+#	$(hide)if [[ $(HOST_OS) == "linux" ]]; then \
+#		$(call mk-rcp-monitor-atree-file,linux.gtk,x86)    ; \
+#		$(call mk-rcp-monitor-atree-file,linux.gtk,x86_64) ; \
+#	fi
+#	$(hide)if [[ $(HOST_OS) == "darwin" ]]; then \
+#		$(call mk-rcp-monitor-atree-file,macosx.cocoa,x86_64) ; \
+#	fi
+#	$(hide)if [[ $(HOST_OS) == "windows" ]]; then \
+#		$(call mk-rcp-monitor-atree-file,win32.win32,x86)    ; \
+#		$(call mk-rcp-monitor-atree-file,win32.win32,x86_64) ; \
+#	fi
