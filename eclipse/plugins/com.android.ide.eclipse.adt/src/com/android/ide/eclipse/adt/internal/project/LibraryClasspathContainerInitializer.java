e80daeac85a815b60abd950217072c21eab3d2a9
Raphael Moll <ralf@android.com>
2012-04-26 22:24:12 -0700

    Prevent ADT NPE when project has no project.prop file.
    There's an internal NPE when initializing the
    library classpath container if a project has
    neither a default.properties or a project.properties.
    
    See SDK Bug: 29632 for details.
    
    Change-Id: I5e3fda0b84beaf47d59fb102439695a32491e5f8

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/project/LibraryClasspathContainerInitializer.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/project/LibraryClasspathContainerInitializer.java
@@ -172,6 +172,10 @@ public class LibraryClasspathContainerInitializer extends BaseClasspathContainer
 
         // check if the project has a valid target.
         ProjectState state = Sdk.getProjectState(iProject);
+        if (state == null) {
+            // getProjectState should already have logged an error. Just bail out.
+            return null;
+        }
 
         /*
          * At this point we're going to gather a list of all that need to go in the
