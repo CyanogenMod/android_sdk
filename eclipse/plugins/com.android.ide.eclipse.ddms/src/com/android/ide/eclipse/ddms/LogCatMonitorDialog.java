6b86bb4a27b75d2d491012e8d9f13a4d765ad166
Tor Norbye <tnorbye@google.com>
2012-05-09 16:31:43 -0700

    Tweak DDMS monitor dialog
    Change-Id: Iab558863997bb8c5f72c8ef9349719ca003c1724

--- eclipse/plugins/com.android.ide.eclipse.ddms/src/com/android/ide/eclipse/ddms/LogCatMonitorDialog.java
+++ eclipse/plugins/com.android.ide.eclipse.ddms/src/com/android/ide/eclipse/ddms/LogCatMonitorDialog.java
@@ -20,7 +20,6 @@ import com.android.ddmlib.Log.LogLevel;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -36,8 +35,8 @@ import org.eclipse.swt.widgets.Shell;
 public class LogCatMonitorDialog extends TitleAreaDialog {
     private static final String TITLE = "Auto Monitor Logcat";
     private static final String DEFAULT_MESSAGE =
-            "Would you like ADT to automatically monitor logcat output for messages " +
-            "from applications in the workspace?";
+            "Would you like ADT to automatically monitor logcat \n" +
+            "output for messages from applications in the workspace?";
 
     private boolean mShouldMonitor = true;
 
@@ -59,22 +58,25 @@ public class LogCatMonitorDialog extends TitleAreaDialog {
     }
 
     @Override
-    protected Control createDialogArea(Composite shell) {
+    protected Control createDialogArea(Composite parent) {
         setTitle(TITLE);
         setMessage(DEFAULT_MESSAGE);
 
-        Composite parent = (Composite) super.createDialogArea(shell);
+        parent = (Composite) super.createDialogArea(parent);
         Composite c = new Composite(parent, SWT.BORDER);
         c.setLayout(new GridLayout(2, false));
-        c.setLayoutData(new GridData(GridData.FILL_BOTH));
+        GridData gd_c = new GridData(GridData.FILL_BOTH);
+        gd_c.grabExcessVerticalSpace = false;
+        gd_c.grabExcessHorizontalSpace = false;
+        c.setLayoutData(gd_c);
 
         final Button disableButton = new Button(c, SWT.RADIO);
+        disableButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
         disableButton.setText("No, do not monitor logcat output.");
-        GridDataFactory.defaultsFor(disableButton).span(2, 1).applyTo(disableButton);
 
         final Button enableButton = new Button(c, SWT.RADIO);
-        enableButton.setText("Yes, monitor logcat and display logcat view if there are " +
-                             "messages with priority higher than:");
+        enableButton.setText("Yes, monitor logcat and display logcat view if there are\n" +
+                "messages with priority higher than:");
         enableButton.setSelection(true);
 
         final Combo levelCombo = new Combo(c, SWT.READ_ONLY | SWT.DROP_DOWN);
