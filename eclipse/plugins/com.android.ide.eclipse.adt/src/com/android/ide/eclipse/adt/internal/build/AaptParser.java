535af6cbfe810305c75282247a1521dcd17cdfae
Tor Norbye <tnorbye@google.com>
2012-05-16 18:09:20 -0700

    Fix aapt handling for skipped files
    Aapt occasionally emits a "Skipping <file>" message. ADT treated these
    error messages as a fatal error, adding an error marker and setting
    the exit status to fail. The reason for that is that aapt will
    generate this error message when it encounters files in the resource
    directory that start with the letter "_", which is an invalid resource
    name prefix.
    
    However, aapt *also* generates this error message when it encounters
    *benign* files, such as ".git", or emacs backup files such as
    "layout.xml~". Luckily, it includes the "type" of the file in the
    error message.
    
    This changeset looks at the file type, and ignores skipping file
    messages for benign file types.
    
    Change-Id: If735e19d2dca4327be06bde9c6dfdcb2c95d0196

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/build/AaptParser.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/build/AaptParser.java
@@ -152,7 +152,7 @@ public final class AaptParser {
      * invalid, such as a layout file name which starts with _.
      */
     private static final Pattern sSkippingPattern =
-        Pattern.compile("    \\(skipping .+ .+ '(.*)'\\)"); //$NON-NLS-1$
+        Pattern.compile("    \\(skipping (.+) .+ '(.*)'\\)"); //$NON-NLS-1$
 
     /**
      * Suffix of error message which points to the first occurrence of a repeated resource
@@ -411,7 +411,16 @@ public final class AaptParser {
 
             m = sSkippingPattern.matcher(p);
             if (m.matches()) {
-                String location = m.group(1);
+                String location = m.group(2);
+
+                // Certain files can safely be skipped without marking the project
+                // as having errors. See isHidden() in AaptAssets.cpp:
+                String type = m.group(1);
+                if (type.equals("backup")          //$NON-NLS-1$   // main.xml~, etc
+                        || type.equals("hidden")   //$NON-NLS-1$   // .gitignore, etc
+                        || type.equals("index")) { //$NON-NLS-1$   // thumbs.db, etc
+                    continue;
+                }
 
                 // check the values and attempt to mark the file.
                 if (checkAndMark(location, null, p.trim(), osRoot, project,
