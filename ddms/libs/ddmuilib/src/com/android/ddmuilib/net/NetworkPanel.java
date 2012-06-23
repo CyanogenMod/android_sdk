2c6188187236e0147423c4e0cdc2faaeaf40d32c
Michael Wright <michaelwr@google.com>
2012-06-22 18:36:15 -0700

    Fixed DDMS network statistics parsing
    Change-Id: I6238185edd10227a01ae2f5282de18b17edb26bc

--- ddms/libs/ddmuilib/src/com/android/ddmuilib/net/NetworkPanel.java
+++ ddms/libs/ddmuilib/src/com/android/ddmuilib/net/NetworkPanel.java
@@ -750,10 +750,11 @@ public class NetworkPanel extends TablePanel {
                 // iface and set are currently ignored, which groups those
                 // entries together.
                 final NetworkSnapshot.Entry entry = new NetworkSnapshot.Entry();
+
                 entry.iface = null; //cols[1];
-                entry.uid = kernelToTag(cols[3]);
+                entry.uid = Integer.parseInt(cols[3]);
                 entry.set = -1; //Integer.parseInt(cols[4]);
-                entry.tag = (int) (Long.decode(cols[2]) >> 32);
+                entry.tag = kernelToTag(cols[2]);
                 entry.rxBytes = Long.parseLong(cols[5]);
                 entry.rxPackets = Long.parseLong(cols[6]);
                 entry.txBytes = Long.parseLong(cols[7]);
