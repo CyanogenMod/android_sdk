3c8bf0660fbfc4858eed4a78830ce25dd27cafc0
Tor Norbye <tnorbye@google.com>
2012-07-23 18:16:58 -0700

    Fix String#format checks
    Change-Id: I83023903a7744b1372a832dfdc56b595c21f4902

--- lint/libs/lint_checks/src/com/android/tools/lint/checks/StringFormatDetector.java
+++ lint/libs/lint_checks/src/com/android/tools/lint/checks/StringFormatDetector.java
@@ -46,6 +46,7 @@ import org.w3c.dom.NodeList;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
@@ -71,17 +72,19 @@ import lombok.ast.NullLiteral;
 import lombok.ast.Select;
 import lombok.ast.StrictListAccessor;
 import lombok.ast.StringLiteral;
-import lombok.ast.VariableDefinition;
 import lombok.ast.VariableDefinitionEntry;
 import lombok.ast.VariableReference;
 
 /**
  * Check which looks for problems with formatting strings such as inconsistencies between
  * translations or between string declaration and string usage in Java.
+ * <p>
+ * TODO: Handle Resources.getQuantityString as well
  */
 public class StringFormatDetector extends ResourceXmlDetector implements Detector.JavaScanner {
     /** The name of the String.format method */
     private static final String FORMAT_METHOD = "format"; //$NON-NLS-1$
+    private static final String GET_STRING_METHOD = "getString"; //$NON-NLS-1$
 
     /** Whether formatting strings are invalid */
     public static final Issue INVALID = Issue.create(
@@ -767,7 +770,7 @@ public class StringFormatDetector extends ResourceXmlDetector implements Detecto
 
     @Override
     public List<String> getApplicableMethodNames() {
-        return Collections.singletonList(FORMAT_METHOD);
+        return Arrays.asList(FORMAT_METHOD, GET_STRING_METHOD);
     }
 
     @Override
@@ -778,13 +781,29 @@ public class StringFormatDetector extends ResourceXmlDetector implements Detecto
         }
 
         String methodName = node.astName().getDescription();
-        assert methodName.equals(FORMAT_METHOD);
-        if (node.astOperand() instanceof VariableReference) {
-            VariableReference ref = (VariableReference) node.astOperand();
-            if ("String".equals(ref.astIdentifier().astValue())) { //$NON-NLS-1$
-                // Found a String.format call
-                // Look inside to see if we can find an R string
-                // Find surrounding method
+        if (methodName.equals(FORMAT_METHOD)) {
+            // String.format(getResources().getString(R.string.foo), arg1, arg2, ...)
+            // Check that the arguments in R.string.foo match arg1, arg2, ...
+            if (node.astOperand() instanceof VariableReference) {
+                VariableReference ref = (VariableReference) node.astOperand();
+                if ("String".equals(ref.astIdentifier().astValue())) { //$NON-NLS-1$
+                    // Found a String.format call
+                    // Look inside to see if we can find an R string
+                    // Find surrounding method
+                    lombok.ast.Node current = node.getParent();
+                    while (current != null && !(current instanceof MethodDeclaration)) {
+                        current = current.getParent();
+                    }
+                    if (current instanceof MethodDeclaration) {
+                        checkStringFormatCall(context, (MethodDeclaration) current, node);
+                    }
+                }
+            }
+        } else {
+            // getResources().getString(R.string.foo, arg1, arg2, ...)
+            // Check that the arguments in R.string.foo match arg1, arg2, ...
+            if (node.astArguments().size() > 1 && node.astOperand() != null ) {
+                // Multiple arguments: formatted form of the String.format list
                 lombok.ast.Node current = node.getParent();
                 while (current != null && !(current instanceof MethodDeclaration)) {
                     current = current.getParent();
@@ -1036,6 +1055,11 @@ public class StringFormatDetector extends ResourceXmlDetector implements Detecto
                 String reference = ((Select) current).astIdentifier().astValue();
 
                 while (current != mTop && !(current instanceof VariableDefinitionEntry)) {
+                    if (current == mTargetNode) {
+                        mName = reference;
+                        mDone = true;
+                        return false;
+                    }
                     current = current.getParent();
                 }
                 if (current instanceof VariableDefinitionEntry) {
@@ -1097,7 +1121,7 @@ public class StringFormatDetector extends ResourceXmlDetector implements Detecto
             } else if (expression instanceof MethodInvocation) {
                 MethodInvocation method = (MethodInvocation) expression;
                 String methodName = method.astName().astValue();
-                if (methodName.equals("getString")) { //$NON-NLS-1$
+                if (methodName.equals(GET_STRING_METHOD)) {
                     return String.class;
                 }
             } else if (expression instanceof StringLiteral) {
@@ -1113,12 +1137,6 @@ public class StringFormatDetector extends ResourceXmlDetector implements Detecto
             }
 
             return null;
-        }
-
-        @Override
-        public boolean visitVariableDefinition(VariableDefinition node) {
-            // TODO Auto-generated method stub
-            return super.visitVariableDefinition(node);
         }
     }
 }
