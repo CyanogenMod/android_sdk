e662bf4410674c7fca9f8ab08badffac9fa60b40
Michael Wright <michaelwr@google.com>
2012-07-26 16:12:30 -0700

    Add listener interface for changes in device lists
    Allows things like the GLE to be notified when a device is added or
    removed via the AVD Manager so they can update their menus
    accordingly.
    
    Change-Id: I616234e7dba3151712cf0d6df600a9104da4f806

--- sdkmanager/libs/sdklib/src/com/android/sdklib/devices/DeviceManager.java
+++ sdkmanager/libs/sdklib/src/com/android/sdklib/devices/DeviceManager.java
@@ -68,6 +68,8 @@ public class DeviceManager {
     private static List<Device> mUserDevices;
     private static List<Device> mDefaultDevices;
     private static final Object lock = new Object();
+    private static final List<DevicesChangeListener> listeners =
+        Collections.synchronizedList(new ArrayList<DevicesChangeListener>());
 
     // TODO: Refactor this to look more like AvdManager so that we don't have
     // multiple instances
@@ -76,6 +78,34 @@ public class DeviceManager {
     // to.
     public DeviceManager(ISdkLog log) {
         mLog = log;
+    }
+
+    /**
+     * Interface implemented by objects which want to know when changes occur to the {@link Device}
+     * lists.
+     */
+    public static interface DevicesChangeListener {
+        /**
+         * Called after one of the {@link Device} lists has been updated.
+         */
+        public void onDevicesChange();
+    }
+
+    /**
+     * Register a listener to be notified when the device lists are modified.
+     * @param listener The listener to add
+     */
+    public void registerListener(DevicesChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Removes a listener from the notification list such that it will no longer receive
+     * notifications when modifications to the {@link Device} list occur.
+     * @param listener The listener to remove.
+     */
+    public void unregisterListener(DevicesChangeListener listener) {
+        listeners.remove(listener);
     }
 
     /**
@@ -111,6 +141,7 @@ public class DeviceManager {
                     mLog.error(null, "Error reading default devices");
                     mDefaultDevices = new ArrayList<Device>();
                 }
+                notifyListeners();
             }
         }
         return Collections.unmodifiableList(mDefaultDevices);
@@ -125,6 +156,7 @@ public class DeviceManager {
     public List<Device> getVendorDevices(String sdkLocation) {
         synchronized (lock) {
             if (mVendorDevices == null || !mVendorDevicesLocation.equals(sdkLocation)) {
+                mVendorDevicesLocation = sdkLocation;
                 List<Device> devices = new ArrayList<Device>();
                 File extrasFolder = new File(sdkLocation, SdkConstants.FD_EXTRAS);
                 List<File> deviceDirs = getExtraDirs(extrasFolder);
@@ -135,6 +167,7 @@ public class DeviceManager {
                     }
                 }
                 mVendorDevices = devices;
+                notifyListeners();
             }
         }
         return Collections.unmodifiableList(mVendorDevices);
@@ -156,6 +189,7 @@ public class DeviceManager {
                     userDevicesFile = new File(AndroidLocation.getFolder(),
                             SdkConstants.FN_DEVICES_XML);
                     mUserDevices.addAll(DeviceParser.parse(userDevicesFile));
+                    notifyListeners();
                 } catch (AndroidLocationException e) {
                     mLog.warning("Couldn't load user devices: %1$s", e.getMessage());
                 } catch (SAXException e) {
@@ -186,6 +220,7 @@ public class DeviceManager {
             }
             mUserDevices.add(d);
         }
+        notifyListeners();
     }
 
     public void removeUserDevice(Device d) {
@@ -199,6 +234,7 @@ public class DeviceManager {
                 if (userDevice.getName().equals(d.getName())
                         && userDevice.getManufacturer().equals(d.getManufacturer())) {
                     it.remove();
+                    notifyListeners();
                     break;
                 }
 
@@ -331,6 +367,14 @@ public class DeviceManager {
             mLog.error(e, null);
         }
         return new ArrayList<Device>();
+    }
+
+    private void notifyListeners() {
+        synchronized (listeners) {
+            for (DevicesChangeListener listener : listeners) {
+                listener.onDevicesChange();
+            }
+        }
     }
 
     /* Returns all of DeviceProfiles in the extras/ folder */
