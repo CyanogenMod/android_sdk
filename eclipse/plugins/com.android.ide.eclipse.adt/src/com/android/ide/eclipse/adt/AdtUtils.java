4c5c5a8d171538c29787ac7dc5fe35b9e58af129
Tor Norbye <tnorbye@google.com>
2012-08-09 13:29:51 -0700

    Make @TargetApi quickfix use VERSION_CODES instead of ints
    The quickfix to insert a @TargetApi annotation for an API
    violation will now insert
    
        import android.os.Build;    (if necessary)
        ...
        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
    
    etc. Other than the fact that we're using the above format
    in our templates, this also has the advantage that code
    completion and show documentation (F2) with the caret on
    the field will show the compatibility information for
    that release, which isn't the case for raw integers.
    
    Change-Id: I233473bf6df706dc73bc692af7ee059287bc22f1

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/AdtUtils.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/AdtUtils.java
@@ -798,6 +798,39 @@ public class AdtUtils {
     }
 
     /**
+     * Returns the applicable build code (for
+     * {@code android.os.Build.VERSION_CODES}) for the corresponding API level,
+     * or null if it's unknown.
+     *
+     * @param api the API level to look up a version code for
+     * @return the corresponding build code field name, or null
+     */
+    @Nullable
+    public static String getBuildCodes(int api) {
+        // See http://developer.android.com/reference/android/os/Build.VERSION_CODES.html
+        switch (api) {
+            case 1:  return "BASE"; //$NON-NLS-1$
+            case 2:  return "BASE_1_1"; //$NON-NLS-1$
+            case 3:  return "CUPCAKE"; //$NON-NLS-1$
+            case 4:  return "DONUT"; //$NON-NLS-1$
+            case 5:  return "ECLAIR"; //$NON-NLS-1$
+            case 6:  return "ECLAIR_0_1"; //$NON-NLS-1$
+            case 7:  return "ECLAIR_MR1"; //$NON-NLS-1$
+            case 8:  return "FROYO"; //$NON-NLS-1$
+            case 9:  return "GINGERBREAD"; //$NON-NLS-1$
+            case 10: return "GINGERBREAD_MR1"; //$NON-NLS-1$
+            case 11: return "HONEYCOMB"; //$NON-NLS-1$
+            case 12: return "HONEYCOMB_MR1"; //$NON-NLS-1$
+            case 13: return "HONEYCOMB_MR2"; //$NON-NLS-1$
+            case 14: return "ICE_CREAM_SANDWICH"; //$NON-NLS-1$
+            case 15: return "ICE_CREAM_SANDWICH_MR1"; //$NON-NLS-1$
+            case 16: return "JELLY_BEAN"; //$NON-NLS-1$
+        }
+
+        return null;
+    }
+
+    /**
      * Returns the Android version and code name of the given API level
      *
      * @param api the api level
