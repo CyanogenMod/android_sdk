d24bd205c609d8d5d2f85e0b6682102df28be6e9
Roman Nurik <romannurik@google.com>
2012-08-10 15:23:33 -0700

    Fix build.
    Broken by Ic5992773207c92426a9f37409536d74b81dd645b
    
    Change-Id: Ifd19f71c1ebfa1a3aaf8f812eccbadaee27c8f90

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/assetstudio/ConfigureAssetSetPage.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/assetstudio/ConfigureAssetSetPage.java
@@ -1142,7 +1142,7 @@ public class ConfigureAssetSetPage extends WizardPage implements SelectionListen
                 actionBarOptions.theme = mValues.holoDark
                         ? ActionBarIconGenerator.Theme.HOLO_DARK
                                 : ActionBarIconGenerator.Theme.HOLO_LIGHT;
-                actionBarOptions.sourceIsClipart = (mValues.sourceType == CLIPART);
+                actionBarOptions.sourceIsClipart = (mValues.sourceType == SourceType.CLIPART);
 
                 options = actionBarOptions;
                 break;
