507724d3568043e313607003d080c607a01cf7bd
Tor Norbye <tnorbye@google.com>
2012-08-30 08:43:40 -0700

    Also check SimpleDateFormat calls for explicit locale
    This changeset expands on the lint check for calling String
    convenience methods that are locale sensitive without an explicit
    locale (issue 36822). It checks whether a SimpleDateFormat constructor
    that uses the default locale is called, and if so suggests either
    specifying a locale or using one of the pre-configured locale
    sensitive instances of SimpleDateFormat.
    
    Change-Id: Ic4fcb72c2ee6b31d1534b4d909493fef7dcff867

--- lint/libs/lint_checks/src/com/android/tools/lint/checks/LocaleDetector.java
+++ lint/libs/lint_checks/src/com/android/tools/lint/checks/LocaleDetector.java
@@ -16,6 +16,7 @@
 
 package com.android.tools.lint.checks;
 
+import static com.android.tools.lint.detector.api.LintConstants.CONSTRUCTOR_NAME;
 import static com.android.tools.lint.detector.api.LintConstants.FORMAT_METHOD;
 
 import com.android.annotations.NonNull;
@@ -44,6 +45,7 @@ import org.objectweb.asm.tree.analysis.SourceInterpreter;
 import org.objectweb.asm.tree.analysis.SourceValue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -52,7 +54,7 @@ import java.util.List;
  */
 public class LocaleDetector extends Detector implements ClassScanner {
     /** Calling risky convenience methods */
-    public static final Issue ISSUE = Issue.create(
+    public static final Issue STRING_LOCALE = Issue.create(
             "DefaultLocale", //$NON-NLS-1$
             "Finds calls to locale-ambiguous String manipulation methods",
 
@@ -73,6 +75,32 @@ public class LocaleDetector extends Detector implements ClassScanner {
             LocaleDetector.class,
             EnumSet.of(Scope.ALL_RESOURCE_FILES, Scope.CLASS_FILE)).setMoreInfo(
              "http://developer.android.com/reference/java/util/Locale.html#default_locale"); //$NON-NLS-1$
+
+    /** Constructing SimpleDateFormat without an explicit locale */
+    public static final Issue DATE_FORMAT = Issue.create(
+            "SimpleDateFormat", //$NON-NLS-1$
+            "Using SimpleDateFormat directly without an explicit locale",
+
+            "Almost all callers should use `getDateInstance()`, `getDateTimeInstance()`, or " +
+            "`getTimeInstance()` to get a ready-made instance of SimpleDateFormat suitable " +
+            "for the user's locale. The main reason you'd create an instance this class " +
+            "directly is because you need to format/parse a specific machine-readable format, " +
+            "in which case you almost certainly want to explicitly ask for US to ensure that " +
+            "you get ASCII digits (rather than, say, Arabic digits).\n" +
+            "\n" +
+            "Therefore, you should either use the form of the SimpleDateFormat constructor " +
+            "where you pass in an explicit locale, such as Locale.US, or use one of the " +
+            "get instance methods, or suppress this error if really know what you are doing.",
+
+            Category.CORRECTNESS,
+            6,
+            Severity.WARNING,
+            LocaleDetector.class,
+            EnumSet.of(Scope.ALL_RESOURCE_FILES, Scope.CLASS_FILE)).setMoreInfo(
+             "http://developer.android.com/reference/java/text/SimpleDateFormat.html"); //$NON-NLS-1$
+
+    private static final String DATE_FORMAT_OWNER = "java/text/SimpleDateFormat"; //$NON-NLS-1$
+    private static final String STRING_OWNER = "java/lang/String";                //$NON-NLS-1$
 
     /** Constructs a new {@link LocaleDetector} */
     public LocaleDetector() {
@@ -96,15 +124,35 @@ public class LocaleDetector extends Detector implements ClassScanner {
     }
 
     @Override
+    @Nullable
+    public List<String> getApplicableCallOwners() {
+        return Collections.singletonList(DATE_FORMAT_OWNER);
+    }
+
+    @Override
     public void checkCall(@NonNull ClassContext context, @NonNull ClassNode classNode,
             @NonNull MethodNode method, @NonNull MethodInsnNode call) {
         String owner = call.owner;
-        if (!owner.equals("java/lang/String")) {             //$NON-NLS-1$
+        String desc = call.desc;
+        String name = call.name;
+        if (owner.equals(DATE_FORMAT_OWNER)) {
+            if (!name.equals(CONSTRUCTOR_NAME)) {
+                return;
+            }
+            if (desc.equals("(Ljava/lang/String;Ljava/text/DateFormatSymbols;)V")   //$NON-NLS-1$
+                    || desc.equals("()V")                                           //$NON-NLS-1$
+                    || desc.equals("(Ljava/lang/String;)V")) {                      //$NON-NLS-1$
+                Location location = context.getLocation(call);
+                String message = String.format(
+                    "To get local formatting use getDateInstance(), getDateTimeInstance(), " +
+                    "or getTimeInstance(), or use new SimpleDateFormat(String template, " +
+                    "Locale locale) with for example Locale.US for ASCII dates.", name);
+                context.report(DATE_FORMAT, method, location, message, null);
+            }
+        } else if (!owner.equals(STRING_OWNER)) {
             return;
         }
 
-        String desc = call.desc;
-        String name = call.name;
         if (name.equals(FORMAT_METHOD)) {
             // Only check the non-locale version of String.format
             if (!desc.equals("(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;")) { //$NON-NLS-1$
@@ -138,7 +186,7 @@ public class LocaleDetector extends Detector implements ClassScanner {
                         String message =
                             "Implicitly using the default locale is a common source of bugs: " +
                             "Use String.format(Locale, ...) instead";
-                        context.report(ISSUE, method, location, message, null);
+                        context.report(STRING_LOCALE, method, location, message, null);
                     }
                 }
             } catch (AnalyzerException e) {
@@ -150,7 +198,7 @@ public class LocaleDetector extends Detector implements ClassScanner {
                 String message = String.format(
                     "Implicitly using the default locale is a common source of bugs: " +
                     "Use %1$s(Locale) instead", name);
-                context.report(ISSUE, method, location, message, null);
+                context.report(STRING_LOCALE, method, location, message, null);
             }
         }
     }
