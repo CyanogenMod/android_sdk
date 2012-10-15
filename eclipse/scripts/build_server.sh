59b2b27ac8daf54bc32099750a0e2a76940dd642
Raphael Moll <ralf@android.com>
2012-10-15 15:46:20 -0700

    ADT IDE: qualifier for zip file is just the build number.
    Change-Id: I4239c5e8370c028d4077e6158629dd03944f6dd0

--- eclipse/scripts/build_server.sh
+++ eclipse/scripts/build_server.sh
@@ -107,9 +107,12 @@ function build_plugin() {
 }
 
 function build_adt_ide() {
+  # Build the ADT IDE if this runs on Linux.
+  # Qualifier for the zip files is just the build number if available.
   if [[ -z $INTERNAL_BUILD && "Linux" == $(uname) ]]; then
     ADT_IDE_DEST_DIR="$DEST_DIR" \
     ADT_IDE_QUALIFIER="$QUALIFIER" \
+    ADT_IDE_ZIP_QUALIFIER="${BUILD_NUMBER:-$QUALIFIER}" \
     make PRODUCT-sdk-adt_eclipse_ide
   fi
 }
