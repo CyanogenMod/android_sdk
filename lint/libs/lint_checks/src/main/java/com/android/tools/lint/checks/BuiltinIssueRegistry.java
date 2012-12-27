8f753417cdd212619872e317c4cf805b8f3e0dc2
Jon Larimer <jlarimer@google.com>
2012-12-27 14:33:53 -0500

    Remove ExportedActivity check
    Change-Id: I428628956c7790b5206d83310211411c4ea54c68

--- lint/libs/lint_checks/src/main/java/com/android/tools/lint/checks/BuiltinIssueRegistry.java
+++ lint/libs/lint_checks/src/main/java/com/android/tools/lint/checks/BuiltinIssueRegistry.java
@@ -136,7 +136,6 @@ public class BuiltinIssueRegistry extends IssueRegistry {
         issues.add(ManifestOrderDetector.ILLEGAL_REFERENCE);
         issues.add(SecurityDetector.EXPORTED_PROVIDER);
         issues.add(SecurityDetector.EXPORTED_SERVICE);
-        issues.add(SecurityDetector.EXPORTED_ACTIVITY);
         issues.add(SecurityDetector.EXPORTED_RECEIVER);
         issues.add(SecurityDetector.OPEN_PROVIDER);
         issues.add(SecurityDetector.WORLD_READABLE);
