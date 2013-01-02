tadfisher/platform_tools_base

42173: New Lint Check: <Fragment> instead of <fragment>
Change-Id: I84bf0901cb1f0a941b420fc03b6c8373146fdee1

lint/cli/src/test/java/com/android/tools/lint/checks/WrongCaseDetectorTest.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint.checks;
+
+import com.android.tools.lint.detector.api.Detector;
+
+@SuppressWarnings("javadoc")
+public class WrongCaseDetectorTest extends AbstractCheckTest {
+    @Override
+    protected Detector getDetector() {
+        return new MissingIdDetector();
+    }
+
+    public void test() throws Exception {
+        assertEquals(""
+                + "res/layout/case.xml:18: Warning: Invalid tag <Merge>; should be <merge> [MissingId]\n"
+                + "<Merge xmlns:android=\"http://schemas.android.com/apk/res/android\" >\n"
+                + "^\n"
+                + "res/layout/case.xml:20: Warning: Invalid tag <Fragment>; should be <fragment> [MissingId]\n"
+                + "    <Fragment android:name=\"foo.bar.Fragment\" />\n"
+                + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "res/layout/case.xml:21: Warning: Invalid tag <Include>; should be <include> [MissingId]\n"
+                + "    <Include layout=\"@layout/foo\" />\n"
+                + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "res/layout/case.xml:22: Warning: Invalid tag <RequestFocus>; should be <requestFocus> [MissingId]\n"
+                + "    <RequestFocus />\n"
+                + "    ~~~~~~~~~~~~~~~~\n"
+                + "0 errors, 4 warnings\n",
+
+                lintProject("res/layout/case.xml"));
+    }
+}

CyanogenMod/android_sdk

Change-Id: I33f3284368a9dd199b2cd70c023d6a22f2426c00

lint/cli/src/test/java/com/android/tools/lint/checks/WrongCaseDetectorTest.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint.checks;
+
+import com.android.tools.lint.detector.api.Detector;
+
+@SuppressWarnings("javadoc")
+public class WrongCaseDetectorTest extends AbstractCheckTest {
+    @Override
+    protected Detector getDetector() {
+        return new WrongCaseDetector();
+    }
+
+    public void test() throws Exception {
+        assertEquals(""
+                + "res/layout/case.xml:18: Warning: Invalid tag <Merge>; should be <merge> [WrongCase]\n"
+                + "<Merge xmlns:android=\"http://schemas.android.com/apk/res/android\" >\n"
+                + "^\n"
+                + "res/layout/case.xml:20: Warning: Invalid tag <Fragment>; should be <fragment> [WrongCase]\n"
+                + "    <Fragment android:name=\"foo.bar.Fragment\" />\n"
+                + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "res/layout/case.xml:21: Warning: Invalid tag <Include>; should be <include> [WrongCase]\n"
+                + "    <Include layout=\"@layout/foo\" />\n"
+                + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "res/layout/case.xml:22: Warning: Invalid tag <RequestFocus>; should be <requestFocus> [WrongCase]\n"
+                + "    <RequestFocus />\n"
+                + "    ~~~~~~~~~~~~~~~~\n"
+                + "0 errors, 4 warnings\n",
+
+                lintProject("res/layout/case.xml"));
+    }
+}