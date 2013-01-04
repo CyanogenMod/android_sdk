98df0cd625983742fc6dec224b0e2ce8fe6e74f6
Raphael Moll <ralf@android.com>
2013-01-04 11:22:55 -0800

    SDK: Use prebuilds/devtools for manifmerger and jobb JAR libraries.
    Change-Id: Ic57371dcea36a94a1173012e7c252cd8cf317c88

--- jobb/Android.mk
+++ jobb/Android.mk
@@ -15,17 +15,51 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_JAVA_RESOURCE_DIRS := src
+# The jobb code has moved to tools/base/jobb.
+# The rule below uses the prebuilt jobb.jar if found.
+# If not found, it tries to built it using the tools/base sources.
+#
+# TIP: to regenerate a new prebuilt, simply remove it and rebuilt it:
+# $ rm prebuilts/devtools/jobb.jar
+# $ make jobb
+# $ cd prebuilts/devtools && git commit -a ...
 
-LOCAL_JAR_MANIFEST := etc/manifest.txt
-
-LOCAL_JAVA_LIBRARIES := \
-    fat32lib
-
-LOCAL_MODULE := jobb 
+LOCAL_MODULE := jobb
 LOCAL_MODULE_TAGS := debug
+LOCAL_JAVA_LIBRARIES := fat32lib
 
-include $(BUILD_HOST_JAVA_LIBRARY)
-include $(LOCAL_PATH)/etc/Android.mk
+# Note: we need a path relative to this module's path for LOCAL_PREBUILT_JAVA_LIBRARIES
+# and we need a TOPDIR "absolute" path for the make rule. The "../.." part isn't pretty
+# but allows us to satisfy both requirements.
+jobb_rel_jar := ../../prebuilts/devtools/$(LOCAL_MODULE)/$(LOCAL_MODULE)$(COMMON_JAVA_PACKAGE_SUFFIX)
+jobb_abs_jar := $(LOCAL_PATH)/$(jobb_rel_jar)
+
+# Tools/base project name
+jobb_project_name := jobb
+
+# Build the jar if it doesn't exist
+ifeq (,$(wildcard $(jobb_abs_jar)))
+$(info # Building $(jobb_project_name) prebuilt from tools/base using gradle)
+ifeq (,$(wildcard $(TOPDIR)tools/base))
+$(error Missing tools/base directory. You probably need to run 'repo init -g all,-notdefault,tools' or similar)
+endif
+$(jobb_abs_jar):
+	$(hide)cd $(TOPDIR)tools/base && \
+	./gradlew :$(jobb_project_name):jar && \
+	src=`./gradlew :$(jobb_project_name):properties | \
+	     awk 'BEGIN { N=""; V="" } \
+	          /^archivesBaseName:/ { N=$$2 } \
+	          /^version:/ { V=$$2 } \
+	          END { print N "-" V ".jar" }'` && \
+	cd ../.. && \
+	dst=$(dir $(jobb_abs_jar)) && \
+	mkdir -p $$dst && \
+	cp $(TOPDIR)tools/base/$(jobb_project_name)/build/libs/$$src $(jobb_abs_jar) && \
+	mkdir -p $$dst/etc && \
+	cp $(TOPDIR)tools/base/$(jobb_project_name)/etc/{jobb,jobb.bat} $$dst/etc/
+endif
+
+LOCAL_PREBUILT_JAVA_LIBRARIES := $(jobb_rel_jar)
+
+include $(BUILD_HOST_PREBUILT)
 
