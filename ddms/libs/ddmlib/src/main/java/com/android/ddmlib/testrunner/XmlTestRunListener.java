57c517981cce7c25ef6b805fb36986fdd89e6c46
Brett Chabot <brettchabot@android.com>
2013-01-07 11:27:32 -0800

    Add test duration to XmlTestRunListener.java
    Change-Id: Iebc611a252f09e116fc0c94eddb5156b2c74aa12

--- ddms/libs/ddmlib/src/main/java/com/android/ddmlib/testrunner/XmlTestRunListener.java
+++ ddms/libs/ddmlib/src/main/java/com/android/ddmlib/testrunner/XmlTestRunListener.java
@@ -36,9 +36,6 @@ import java.util.TimeZone;
  * Writes JUnit results to an XML files in a format consistent with
  * Ant's XMLJUnitResultFormatter.
  * <p/>
- * Unlike Ant's formatter, this class does not report the execution time of
- * tests.
- * <p/>
  * Creates a separate XML file per test run.
  * <p/>
  */
@@ -211,7 +208,8 @@ public class XmlTestRunListener implements ITestRunListener {
         serializer.startTag(ns, TESTCASE);
         serializer.attribute(ns, ATTR_NAME, testId.getTestName());
         serializer.attribute(ns, ATTR_CLASSNAME, testId.getClassName());
-        serializer.attribute(ns, ATTR_TIME, "0");
+        long elapsedTimeMs = testResult.getEndTime() - testResult.getStartTime();
+        serializer.attribute(ns, ATTR_TIME, Long.toString(elapsedTimeMs));
 
         if (!TestStatus.PASSED.equals(testResult.getStatus())) {
             String result = testResult.getStatus().equals(TestStatus.FAILURE) ? FAILURE : ERROR;
