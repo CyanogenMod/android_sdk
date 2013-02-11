3cb8b74bb038f7b0ec5606456d63a58bafd7dbb6
Raphael Moll <ralf@android.com>
2013-02-11 14:58:41 -0800

    SDK Manager app: use src/{main,test}/java pattern.
    Just for consistency with our new library overlords.
    
    Change-Id: I5ac78e7d19c9423f0e02177dce1b8644885e5d84

--- sdkmanager/app/Android.mk
+++ sdkmanager/app/Android.mk
@@ -1,10 +1,22 @@
 # Copyright 2007 The Android Open Source Project
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_JAVA_RESOURCE_DIRS := src
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
+LOCAL_JAVA_RESOURCE_DIRS := src/main/java
 
 LOCAL_JAR_MANIFEST := etc/manifest.txt
 
@@ -17,6 +29,7 @@ LOCAL_JAR_MANIFEST := etc/manifest.txt
 #  current VM is 32 or 64 bit.)
 LOCAL_JAVA_LIBRARIES := \
 	common \
+	guava-tools \
 	sdklib \
 	sdkuilib \
 	swt \
@@ -25,10 +38,33 @@ LOCAL_JAVA_LIBRARIES := \
 	org.eclipse.core.commands_3.6.0.I20100512-1500
 
 LOCAL_MODULE := sdkmanager
-LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_JAVA_LIBRARY)
 
 # Build all sub-directories
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+
+# ----- TESTS ------
+# Copyright (C) 2011 The Android Open Source Project
+
+include $(CLEAR_VARS)
+
+# Only compile source java files in this lib.
+LOCAL_SRC_FILES := $(call all-java-files-under, src/test/java)
+
+LOCAL_MODULE := sdkmanager-tests
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_JAVA_LIBRARIES := \
+    guava-tools \
+    httpclient-4.1.1 \
+    httpcore-4.1 \
+    httpmime-4.1.1 \
+    junit \
+    sdkmanager \
+    sdklib
+
+include $(BUILD_HOST_JAVA_LIBRARY)
 
