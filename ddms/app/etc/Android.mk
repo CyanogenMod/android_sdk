f6a5b596f5473558ecc859e8af931a9f1b80a7ea
Raphael Moll <ralf@android.com>
2013-03-12 10:39:00 -0700

    SDK: Delete projects moved to tools/base or tools/swt.
    Change-Id: Iba15f82cb00d19217382c78d8ff37dda1e97ea59

--- ddms/app/etc/Android.mk
+++
@@ -1,8 +0,0 @@
-# Copyright 2007 The Android Open Source Project
-#
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_EXECUTABLES := ddms
-LOCAL_MODULE_TAGS := debug
-include $(BUILD_HOST_PREBUILT)
--- traceview/etc/Android.mk
+++
@@ -1,9 +0,0 @@
-# Copyright 2007 The Android Open Source Project
-#
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_EXECUTABLES := traceview
-LOCAL_MODULE_TAGS := debug
-include $(BUILD_HOST_PREBUILT)
-
