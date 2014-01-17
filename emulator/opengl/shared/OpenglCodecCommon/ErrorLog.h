bf5b0bc2d53d82bc6ff62ceec2fdaa8b796aa130
David 'Digit' Turner <digit@google.com>
2014-01-17 16:47:15 +0100

    emulator/opengl: Refactor SmartPtr implementation.
    This gets rid of two copies of SmartPtr.h and replaces them with
    a single implementation under shared/emugl/common/smart_ptr.*
    
    Note that this uses a new include path rooted at the shared/
    directory for classes that are likely to be built both for
    the host and the device (in case we back-port this to
    device/generic/goldfish/opengl/ in the future).
    
    + Add a gtest-based set of unittests, after building, just
      call 'emugl_common_host_unittests' to run it.
    
      Note that this probably needs a 64-bit version as well,
      will come later once I find a way to build GTest for 64-bits
      without breaking the platform build :-)
    
    Also note that this moves the class to the 'emugl' namespace,
    in order to make the code easier to build out of the platform
    tree, and embed it in other projects. More classes will be
    transitioned / refactored in future patches.
    
    AOSP_BUG=64806
    
    Change-Id: Ieb326c5f3f002a21537b8a391a82ce2ef9925073

--- emulator/opengl/shared/OpenglCodecCommon/ErrorLog.h
+++ emulator/opengl/shared/OpenglCodecCommon/ErrorLog.h
@@ -16,7 +16,7 @@
 #ifndef _ERROR_LOG_H_
 #define _ERROR_LOG_H_
 
-#if (HAVE_ANDROID_OS == 1)
+#ifdef __ANDROID__
 #    include <cutils/log.h>
 #    define ERR(...)    ALOGE(__VA_ARGS__)
 #    ifdef EMUGL_DEBUG
--- emulator/opengl/shared/OpenglCodecCommon/GLClientState.cpp
+++ emulator/opengl/shared/OpenglCodecCommon/GLClientState.cpp
@@ -19,7 +19,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "glUtils.h"
-#include <cutils/log.h>
 
 #ifndef MAX
 #define MAX(a, b) ((a) < (b) ? (b) : (a))
